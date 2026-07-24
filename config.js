1804790b6293fe45d5f101a47d9d14972430c88c
Juliana Oliveira <julianalo@praiagrande.sp.gov.br>
2026-07-24 11:09:33 -0300

    chore: update main

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Arquivo local — NÃO versionar (está no .gitignore).
 window.AGENDACONFIG = {
-  CLIENT_ID: "996587016973-q4ejsv0jsscp867fdlq6ebdflhidn0d2.apps.googleusercontent.com",
+  CLIENT_ID: "999069950449-9i9pe1l5qvg09cvs0mtpli3s59i6p27r.apps.googleusercontent.com",
   SPREADSHEET_ID: "12B32YPPSnTrhSrV3u8tn87nd5nfKssv_vuUAe4_4YBQ",
   SHEET_NAME: "Prefeito (Em testes)",
   SCOPES: "https://www.googleapis.com/auth/spreadsheets",
